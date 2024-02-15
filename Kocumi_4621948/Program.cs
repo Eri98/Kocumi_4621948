c14980e31ffa412c892c2daa6aed4edd3a741a7d
Ergis Kocumi <ergis.kocumi.majo@gmail.com>
2024-02-15 10:36:08 +0100

    Add files via upload

---
+++ Kocumi_4621948/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Secondo_Appello_TAP;
+
+public static class Extention_Method
+{
+    public static void Main()
+    {
+
+    }
+}
---
+++ Kocumi_4621948/README.md
@@ -0,0 +1,4 @@
+# appello-2-Eri98
+# appello-2-Eri98
+# appello-2-Eri98
+# appello-2-Eri98
